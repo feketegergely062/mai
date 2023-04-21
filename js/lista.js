22422777b3ba9fede65e569b4d7841dc4c5e828c
Fekete Gergely <feketegergely062@gmail.com>
2023-04-21 15:05:56 +0200

    torles

--- js/lista.js
+++ js/lista.js
@@ -28,3 +28,26 @@ addForm.addEventListener('submit', function(evet){
     Telefonszam.value = '';
 
 });
+function torol(){
+    const row = document.createElement('tr');
+    const vezeteknevCell = document.createElement('td');
+    const keresztnevCell = document.createElement('td');
+    const telefonszamCell = document.createElement('td');
+
+    vezeteknevCell.textContent = Vezeteknev.value;
+    keresztnevCell.textContent = Keresztnev.value;
+    telefonszamCell.textContent = Telefonszam.value;
+
+    row.appendChild(vezeteknevCell);
+    row.appendChild(keresztnevCell);
+    row.appendChild(telefonszamCell);
+
+    dataTable.appendChild(row);
+
+    Vezeteknev.value = '';
+    Keresztnev.value = '';
+    Telefonszam.value = '';
+
+    
+
+}
