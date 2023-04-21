649ca3eb463ff455dcb50ed6850c35ec3a38babd
Fekete Gergely <feketegergely062@gmail.com>
2023-04-21 14:13:14 +0200

    hozzad

--- js/lista.js
+++ js/lista.js
@@ -1,4 +1,30 @@
 const dataTable = document.getElementById('data-table');
 const addForm = document.getElementById('add-form');
 
-const Vezeteknev = document.getElementById('')
+const Vezeteknev = document.getElementById('vezeteknev-input');
+const Keresztnev = document.getElementById('keresztnev-input');
+const Telefonszam = document.getElementById('telefonszam-input');
+
+addForm.addEventListener('submit', function(evet){
+    evet.preventDefault();
+
+    const row = document.createElement('tr');
+    const vezeteknevCell = document.createElement('td');
+    const keresztnevCell = document.createElement('td');
+    const telefonszamCell = document.createElement('td');
+
+    vezeteknevCell.textContent = Vezeteknev.value;
+    keresztnevCell.textContent = Keresztnev.value;
+    telefonszamCell.textContent = Telefonszam.value;
+
+    row.appendChild(vezeteknevCell);
+    row.appendChild(keresztnevCell);
+    row.appendChild(telefonszamCell);
+
+    dataTable.appendChild(row);
+
+    Vezeteknev.value = '';
+    Keresztnev.value = '';
+    Telefonszam.value = '';
+
+});
